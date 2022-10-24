bc79f5b7ab653a450079d223b510fcae7c2c1c44
BuildEnough <rhtmaehcl878@naver.com>
2022-10-24 15:33:43 +0900

    CRUD forms vaild

---
+++ 20221024_1/articles/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'articles'
+
+urlpatterns = [
+    path('index/', views.index, name='index'),
+    path('create/', views.create, name='create'),
+    path('new/', views.new, name='new'),
+    path('<int:article_pk>/delete', views.delete, name='delete'),
+    path('<int:article_pk>/detail', views.detail, name='detail'),
+    path('<int:article_pk>/update', views.update, name='update'),
+]
