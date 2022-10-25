11384b9c091dfa730f6e238daa4c007fd9e0cf7d
BuildEnough <rhtmaehcl878@naver.com>
2022-10-25 17:07:23 +0900

    templates filter, admin

---
+++ 20221025_1/articles/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, redirect
+from .models import Articles
+from .forms import ArticleForm
+# Create your views here.
+
+def index(request):
+    articles = Articles.objects.order_by('-pk')
+    context = {
+        'articles': articles
+    }
+    return render(request, 'articles/index.html', context)
+
+def new(request):
+    if request.method == 'POST':
+        article_form  = ArticleForm(request.POST)
+        if article_form.is_valid():
+            article_form.save()
+            return redirect('articles:index')
+    else:
+        article_form = ArticleForm()
+    context = {
+        'article_form': article_form
+    }
+    return render(request, 'articles/form.html', context)
+
+def delete(request, article_pk):
+    article = Articles.objects.get(pk=article_pk)
+    article.delete()
+    return redirect('articles:index')
+
+def detail(request, article_pk):
+    article = Articles.objects.get(pk = article_pk)
+    context = {
+        'article': article
+    }
+    return render(request, 'articles/detail.html', context)
+
+def update(request, article_pk):
+    article = Articles.objects.get(pk=article_pk)
+    if request.method == 'POST':
+        article_form  = ArticleForm(request.POST, instance=article)
+        if article_form.is_valid():
+            article_form.save()
+            return redirect('articles:index')
+    else:
+        article_form = ArticleForm(instance=article)
+    context = {
+        'article_form': article_form
+    }
+    return render(request, 'articles/form.html', context)
