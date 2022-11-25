5feb3bc525fc6d86f0c7f03aab1db4f3520ba1a8
BuildEnough <rhtmaehcl878@naver.com>
2022-11-26 00:02:05 +0900

    base: Model 설정

--- project1/projects/models.py
+++ project1/projects/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Projects(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField(max_length=70)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
